0e0883b4c94301030bae0a9a430a62ada8b03a2b
David Riusech <v1X3Q0@gmail.com>
2021-11-23 23:13:33 -0500

    building on linux

--- localUtil.cpp
+++ localUtil.cpp
@@ -1,4 +1,6 @@
 #include <string.h>
+#include <stdio.h>
+#include <stdint.h>
 
 size_t rstrnlen(const char* s, size_t maxlen)
 {
@@ -43,4 +45,49 @@ int rstrncmp(const char* s1, const char* s2, size_t maxlen)
         }
     }
     return 0;
+}
+
+template<typename unitLen>
+void dumpMemT(unsigned char* base, size_t len, const char* format)
+{
+   size_t i = 0;
+   printf("%04hx: ", (unsigned short)i);
+   for (i = 0; i < len; i += (sizeof(unitLen)))
+   {
+      if (((i % 0x10) == 0) && (i != 0))
+      {
+         printf("\n");
+         printf("UD:%04hx: ", (unsigned short)i);
+      }
+      printf(format, *((unitLen*)&base[i]));
+   }
+}
+
+void dumpMem(uint8_t* base, size_t len, char format)
+{
+   printf("UD:");
+   if (format == 's')
+   {
+      printf(" %s", (char*)base);
+   }
+   else
+   {
+      if (format == 'c')
+      {
+         dumpMemT<uint8_t>(base, len, "0x%02hhx ");
+      }
+      else if (format == 'h')
+      {
+         dumpMemT<uint16_t>(base, len, "0x%04hx ");
+      }
+      else if (format == 'w' || ((format == 0) && (sizeof(void*) == 4)))
+      {
+         dumpMemT<uint32_t>(base, len, "0x%08x ");
+      }
+      else if (format == 'q' || ((format == 0) && (sizeof(void*) == 8)))
+      {
+         dumpMemT<uint64_t>(base, len, "0x%016lx ");
+      }
+   }
+   printf("\n");
 }
--- localUtil.h
+++ localUtil.h
@@ -1,10 +1,25 @@
 #ifndef LOCALUTIL
 #define LOCALUTIL
 
+#include <stdint.h>
+#include <stdio.h>
+
 #define PAGE_SIZE   0x1000
 #define PAGE_MASK   (PAGE_SIZE - 1)
 
 #define DEFAULT_SEARCH_SIZE     0x80
+
+#define FINISH_IF(x) \
+    if (x) \
+    { \
+        goto finish; \
+    }
+
+#define SAFE_BAIL(x) \
+    if (x) \
+    { \
+        goto fail; \
+    }
 
 #define SAFE_CLOSE(x) \
     if (x) \
@@ -30,5 +45,6 @@
 size_t rstrnlen(const char* s, size_t maxlen);
 size_t rfindnn(const char* s, size_t maxlen);
 int rstrncmp(const char* s1, const char* s2, size_t maxlen);
+void dumpMem(uint8_t* base, size_t len, char format);
 
 #endif
